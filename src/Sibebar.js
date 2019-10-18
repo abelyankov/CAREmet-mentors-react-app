0d5fb225a1b845f69d8402b9b5d1df7245251f8a
Arthur <arthurbelyankove@gmail.com>
2019-10-19 02:00:30 +0600

    sidebar updated
    Страницы (6): Профиль - логин, наставники - просмотр наставников, отчёты - просмотр отчёта.
    Если в профиле нажал на выход - отправляем на стейт с логином. Как-то так.

--- src/Sibebar.js
+++ src/Sibebar.js
@@ -11,28 +11,10 @@ function Sidebar() {
                 Профиль
             </a>
             <a className="item">
-                Заявки наставников
-            </a>
-            <a className="item">
                 Наставники
             </a>
             <a className="item">
-                Чат
-            </a>
-            <a className="item">
                 Отчёты
-            </a>
-            <a className="item">
-                Встречи
-            </a>
-            <a className="item">
-                Детские дома
-            </a>
-            <a className="item">
-                Дети
-            </a>
-            <a className="item">
-                Кураторы
             </a>
         </div>
     );

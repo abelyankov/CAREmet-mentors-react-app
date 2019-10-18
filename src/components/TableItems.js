15abeb4256e633831fac4585284bd04e7568e626
paydaylight <maxat235@gmail.com>
2019-10-19 02:52:32 +0600

    sum changez

---
+++ src/components/TableItems.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class TableItems extends React.Component {
+    render() {
+        return(
+            <div>
+                <tr>
+                    <td>Наставник</td>
+                    <td>Ребенок</td>
+                    <td>Куратор</td>
+                    <td>Психолог</td>
+                    <td>Город</td>
+                    <td>Статус</td>
+                    <td>Детский дом</td>
+                    <td>Отчёты </td>
+                </tr>
+            </div>
+        )
+    }
+}
+
+export default TableItems

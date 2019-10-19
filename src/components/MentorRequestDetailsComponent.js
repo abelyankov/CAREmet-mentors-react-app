84eb748fe21813c9dcc5e718bf2cb451c388f7a4
Rustam <rses54@gmail.com>
2019-10-19 15:15:27 +0600

    mentor request details verstka

--- src/components/MentorRequestDetailsComponent.js
+++ src/components/MentorRequestDetailsComponent.js
@@ -16,15 +16,50 @@ class MentorRequestDetailsComponent extends React.Component{
 
                 <Sidebar />
 
-                <h1>Name: {mentorRequest.name}</h1>
-                <h1>Age: {mentorRequest.age}</h1>
-                <h1>City: {mentorRequest.city}</h1>
-                <h1>Phone number: {mentorRequest.phoneNumber}</h1>
-                <h1>Work: {mentorRequest.work}</h1>
-                <h1>Work Position: {mentorRequest.workPosition}</h1>
-                <h1>Hobby: {mentorRequest.hobby}</h1>
-                <h1>About : {mentorRequest.aboutYourSelf}</h1>
-
+                <h1>Name: {mentorRequest.name} {mentorRequest.surname}</h1>
+                {/*<h1>Age: {mentorRequest.age}</h1>*/}
+                {/*<h1>City: {mentorRequest.city}</h1>*/}
+                {/*<h1>Phone number: {mentorRequest.phoneNumber}</h1>*/}
+                {/*<h1>Work: {mentorRequest.work}</h1>*/}
+                {/*<h1>Work Position: {mentorRequest.workPosition}</h1>*/}
+                {/*<h1>Hobby: {mentorRequest.hobby}</h1>*/}
+                {/*<h1>About : {mentorRequest.aboutYourSelf}</h1>*/}
+                <div className="ui segment" style={{background: "rgba(0,0,0,0.03)", margin: "20px"}}>
+                    <table className="ui very basic compact table">
+                        <tbody>
+                        <tr>
+                            <td style={{width: "180px"}}>
+                                Возраст
+                            </td>
+                            <td> {mentorRequest.age}</td>
+                        </tr>
+                        <tr>
+                            <td style={{width: "180px"}}>Город</td>
+                            <td> {mentorRequest.city} </td>
+                        </tr>
+                        <tr>
+                            <td style={{width: "180px"}}>Phone number</td>
+                            <td> {mentorRequest.phoneNumber} </td>
+                        </tr>
+                        <tr>
+                            <td style={{width: "180px"}}>Work</td>
+                            <td> {mentorRequest.work} </td>
+                        </tr>
+                        <tr>
+                            <td style={{width: "180px"}}>Work Position</td>
+                            <td> {mentorRequest.workPosition} </td>
+                        </tr>
+                        <tr>
+                            <td style={{width: "180px"}}>Hobby</td>
+                            <td> {mentorRequest.hobby} </td>
+                        </tr>
+                        <tr>
+                            <td style={{width: "180px"}}>About</td>
+                            <td> {mentorRequest.aboutYourSelf} </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
 
             </div>
         )
